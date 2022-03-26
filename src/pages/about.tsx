93a0aed32f04970469ccaa241f06bd45c177a4b4
Yujeong Yun <yjc.yun@gmail.com>
2022-03-26 01:11:27 -0400

    :sparkles: Add about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const About = () => {
+  return (
+    <Layout>
+      <p>Hi =) </p>
+      <br />
+      <p>
+        I do frontend stuff at work but I'm always curious about the backend and
+        other areas of software development. I can be ambitious and bold but
+        often I'm not. I am easy-going with others but hardcore with myself. I
+        enjoy hiking with my dogs in my free time.
+      </p>
+      <br />
+      <p>
+        I'm currently not available for any freelance work but thank you for
+        asking. Hope you all have a great day =)
+      </p>
+    </Layout>
+  );
+};
+
+export default About;
